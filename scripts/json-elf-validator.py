bbc00c44f729df0dbcf213f45bb3ba66f073dc4e
Dan McNulty <dkmcnulty@gmail.com>
2014-02-12 21:52:26 -0700

    Beginnings of script to help validate ELF parsing results.

--- scripts/json-elf-validator.py
+++ scripts/json-elf-validator.py
@@ -14,27 +14,33 @@ constantMapping = {
     "EXEC (Executable file)" : "ET_EXEC"
 }
 
+READELF_HEADERS = "readelf -h $file"
+SECTION_NAME = "readelf -t $file | gawk 'match($0, /^[[:space:]]+[[[:space:]]*{}][[:space:]]+(.*)/, m) { print m[1] }'"
+NAME_VARIABLE = "NAME=`" + SECTION_NAME + "`; echo name= $NAME >&2;"
+SIZE_VARIABLE = "SIZE=`objdump -h $file | egrep [[:space:]]+${NAME}[[:space:]]+ | awk '{ print $3 }'`;"
+OFFSET_VARIABLE = "OFFSET=`objdump -h $file | egrep [[:space:]]+${NAME}[[:space:]]+ | awk '{ print $6 }'`;"
+
 jsonPathMappings = {
-     ".header.entry.value" : "readelf -h $file | gawk 'match($0, /Entry point address:[[:space:]]+(.*)/, m) { print m[1] }'",
-     ".header.fileType" : "readelf -h $file | gawk 'match($0, /Type:[[:space:]]+(.*)/, m) { print m[1] }'",
+     ".header.entry.value" : READELF_HEADERS + " | gawk 'match($0, /Entry point address:[[:space:]]+(.*)/, m) { print m[1] }'",
+     ".header.fileType" : READELF_HEADERS + " | gawk 'match($0, /Type:[[:space:]]+(.*)/, m) { print m[1] }'",
 #    ".header.flags" : "",
-     ".header.headerSize" : "readelf -h $file | gawk 'match($0, /Size of this header:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.headerSize" : READELF_HEADERS + " | gawk 'match($0, /Size of this header:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
 #    ".header.ident.dataEncoding" : "",
-     ".header.ident.elfClass" : "readelf -h $file | gawk 'match($0, /Class:[[:space:]]+(.*)/, m) { print m[1] }'",
-     ".header.ident.fileIdentification" : "readelf -h $file | gawk 'match($0, /Magic:[[:space:]]+(.*)/, m) { print m[1] }' | gawk '{ print $1 " " $2 " " $3 " " $4 }' | xxd -r -p | base64",
+     ".header.ident.elfClass" : READELF_HEADERS + " | gawk 'match($0, /Class:[[:space:]]+(.*)/, m) { print m[1] }'",
+     ".header.ident.fileIdentification" : READELF_HEADERS + " | gawk 'match($0, /Magic:[[:space:]]+(.*)/, m) { print m[1] }' | gawk '{ print $1 " " $2 " " $3 " " $4 }' | xxd -r -p | base64 --wrap=0",
 #    ".header.ident.idVersion" : "",
 #    ".header.ident.padding" : "",
 #    ".header.machineType" : "",
-     ".header.numProgramHeaders" : "readelf -h $file | gawk 'match($0, /Number of program headers:[[:space:]]+(.*)/, m) { print m[1] }'",
-     ".header.numSectionHeaders" : "readelf -h $file | gawk 'match($0, /Number of section headers:[[:space:]]+(.*)/, m) { print m[1] }'",
-     ".header.programHeaderOffset.value" : "readelf -h $file | gawk 'match($0, /Start of program headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
-     ".header.programHeaderSize" : "readelf -h $file | gawk 'match($0, /Size of program headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
-     ".header.sectionHeaderOffset.value" : "readelf -h $file | gawk 'match($0, /Start of section headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
-     ".header.sectionHeaderSize" : "readelf -h $file | gawk 'match($0, /Size of section headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
-     ".header.sectionNameStrIndex" : "readelf -h $file | gawk 'match($0, /Section header string table index:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.numProgramHeaders" : READELF_HEADERS + " | gawk 'match($0, /Number of program headers:[[:space:]]+(.*)/, m) { print m[1] }'",
+     ".header.numSectionHeaders" : READELF_HEADERS + " | gawk 'match($0, /Number of section headers:[[:space:]]+(.*)/, m) { print m[1] }'",
+     ".header.programHeaderOffset.value" : READELF_HEADERS + " | gawk 'match($0, /Start of program headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.programHeaderSize" : READELF_HEADERS + " | gawk 'match($0, /Size of program headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.sectionHeaderOffset.value" : READELF_HEADERS + " | gawk 'match($0, /Start of section headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.sectionHeaderSize" : READELF_HEADERS + " | gawk 'match($0, /Size of section headers:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
+     ".header.sectionNameStrIndex" : READELF_HEADERS + " | gawk 'match($0, /Section header string table index:[[:space:]]+(.*)/, m) { print m[1] }' | awk '{ print $1 }'",
 #    ".header.version" : "",
-     ".sections[{}].name": "readelf -t $file | gawk 'match($0, /^[[:space:]]+[[[:space:]]*{}][[:space:]]+(.*)/, m) { print m[1] }'",
-#    ".sections[{}].sectionContent.data" : "",
+     ".sections[{}].name": SECTION_NAME,
+#    ".sections[{}].sectionContent.data" : NAME_VARIABLE + SIZE_VARIABLE + OFFSET_VARIABLE + "dd if=$file ibs=1c skip=`expr $(( 0x$OFFSET ))` count=`expr $(( 0x$SIZE ))` 2>/dev/null | base64 --wrap=0",
 #    ".sections[{}].sectionContent.entries[{}].tag" : "",
 #    ".sections[{}].sectionContent.entries[{}].tagValue.value" : "",
 #    ".sections[{}].sectionContent.entries[{}].value.value" : "",
@@ -81,7 +87,7 @@ def replaceVars(path, filePath, params):
 def matchOutput(currentPath, value, cmd):
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     output = proc.communicate()[0]
-    if str(value) != output.strip():
+    if str(value) != str(output.strip()):
         if type(value) is int:
             # try in hex
             if str(hex(value)) == output.strip():
@@ -90,7 +96,7 @@ def matchOutput(currentPath, value, cmd):
         if output.strip() in constantMapping:
             if str(value) == constantMapping[output.strip()]:
                 return
-        raise ValueError(currentPath + ": " + str(value) + " != " + output.strip())
+        raise ValueError(currentPath + ": '" + str(value) + "' != '" + output.strip() + "'")
 
 def validateJsonValues(payload, filePath, pathMappings, currentPath, params):
     if type(payload) is dict:
